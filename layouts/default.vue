17235b213916f57dfe3e8a2ff71b8e09e631c907
Jefre Massingue <jefremasa@gmail.com>
2023-12-04 21:48:52 +0200

    chore: added code to hide loader on pages with no animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,11 @@
 
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-
+try {
+        document.querySelector('#loader').classList.remove('hidden')
+    } catch (error) {
+        
+    }
 onMounted(() => {
     AOS.init();
     try {
